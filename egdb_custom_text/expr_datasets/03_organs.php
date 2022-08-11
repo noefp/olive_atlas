ae7ff8cef0fbbec63ac58ecd58c2ac2f7991531a
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2022-08-11 17:14:43 +0200

    dataset description

---
+++ egdb_custom_text/expr_datasets/03_organs.php
@@ -0,0 +1,16 @@
+<p>
+  RNA-seq of roots, stems, meristems, leaves, flowers, and fruits (epicarp and mesocarp)
+  of three healthy 10-year-old olive trees under field conditions. All the samples were collected at the same time,
+  except for fruits, which had to be collected later upon reaching the turning ripening state. 
+  <br>
+  <br>
+  The plant samples employed for this study were obtained from the World Olive Germplasm Collection (WOGC) 
+  of the Andalusian Institute of Agricultural and Fisheries Research and Training (IFAPA).
+  cDNA libraries were sequenced by PE sequencing (100 x 2) with an Illumina HiSeq 2000 sequencer and normalized to TPM.
+  <br>
+  <br>
+  These dataset was published in <a href="https://doi.org/10.3390/genes11050544" target="_blank">Ramírez-Tejero et al., 2020</a> and <a href="https://www.ncbi.nlm.nih.gov/bioproject/PRJNA590386/" target="_blank">BioProject PRJNA590386</a>
+</p>
+
+<br>
+
