e69a2227fffe740d6f649d04a926e37cd72957d6
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2022-08-11 15:54:58 +0200

    new conf variables

--- egdb_conf/easyGDB_conf.php
+++ egdb_conf/easyGDB_conf.php
@@ -1,16 +1,17 @@
 <?php
 // File paths
 $root_path = "/var/www/html"; //use absolute path
+$egdb_files_folder = "olive_atlas";
 $easy_gdb_path = "$root_path/easy_gdb";
 $blast_dbs_path = "$root_path/blast_dbs";
 $expression_path = "$root_path/expression_data";
 $downloads_path = "downloads";
 $lookup_path = "$root_path/lookup"; //from root 
-$images_path = "/olive_atlas/egdb_images";
-$custom_text_path = "$root_path/olive_atlas/egdb_custom_text";
-$species_path = "$root_path/olive_atlas/egdb_species";
-$lab_path = "$root_path/olive_atlas/egdb_labs";
-$annotation_links_path = "$root_path/olive_atlas/annotations";
+$images_path = "/$egdb_files_folder/egdb_images";
+$custom_text_path = "$root_path/$egdb_files_folder/egdb_custom_text";
+$species_path = "$root_path/$egdb_files_folder/egdb_species";
+$lab_path = "$root_path/$egdb_files_folder/egdb_labs";
+$annotation_links_path = "$root_path/$egdb_files_folder/annotations";
 
 // header
 $dbTitle = "OliveAtlas";
@@ -22,6 +23,7 @@ $tb_about = 1;
 $tb_downloads = 1;
 $tb_species = 0;
 $tb_search_box = 0;
+$tb_tools = 0;
 $tb_search = 0;
 $tb_blast = 0;
 $tb_jbrowse = 0;
@@ -31,6 +33,10 @@ $tb_gene_expr = 0;
 $tb_lookup = 1;
 $tb_more = 0;
 $tb_custom = 1;
+
+// Expression Atlas
+$expr_cards = 1;
+$expr_menu = 1;
 
 // About
 $ab_citation = 1;
@@ -42,16 +48,6 @@ $input_gene_list="gene1.1
 gene2.1
 gene3.1";
 
-$expr_input_gene_list="gene1
-gene2
-gene3
-gene4
-gene5
-gene6
-gene7
-gene8
-gene9
-gene10";
 
 // Tools
 $max_lookup_input = 10000;
