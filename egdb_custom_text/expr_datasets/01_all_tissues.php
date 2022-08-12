5ca40765dd68976c6051d8fb4b775a2b8340ef28
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2022-08-12 17:06:46 +0200

    fixed citation

--- egdb_custom_text/expr_datasets/01_all_tissues.php
+++ egdb_custom_text/expr_datasets/01_all_tissues.php
@@ -19,7 +19,7 @@
   </ul>
 <p>
   <span style="color:green;">Leaves and Roots</span> (in green) are control samples from 4 month old potted plants in conditions as published by <a href="https://doi.org/10.1093/dnares/dsu033" target="_blank">Leyva-Pérez et al., 2015</a>.
-  <span style="color:blue;">Seed, Embryo and Testa+endosperm</span> (in blue) are control samples in conditions as published by <a href="#" target="_blank">Bullones et al., 2022. In preparation</a>.
+  <span style="color:blue;">Seed, Embryo and Testa+endosperm</span> (in blue) are control samples in conditions as published by <a href="#" target="_blank">??? et al., 2022. In preparation</a>.
   <span style="color:orange;">Pollen</span> samples correspond to mature pollen in conditions as published by <a href="#" target="_blank">Bullones et al., 2022. In preparation</a>.
   The rest of the tissues (in black) come from three healthy 10-year-old olive trees under field conditions. They were published by <a href="https://doi.org/10.3390/genes11050544" target="_blank">Ramírez-Tejero et al., 2020</a>.
 </p>
