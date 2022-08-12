677e94def7c93047bba5953a010115c731355236
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2022-08-12 12:42:33 +0200

    dataset description

---
+++ egdb_custom_text/expr_datasets/01_all_tissues.php
@@ -0,0 +1,30 @@
+<p>
+  RNA-seq of roots, stem, meristem, leaves, flowers, pollen, fruits (epicarp + mesocarp), seeds, embryo, and testa+endosperm,
+  combined from several experiments.
+</p>
+<h4>Experimental Conditions (all in control conditions with 2 replicates unless otherwise noted):</h4>
+  <ul>
+    <li>Roots.</li>
+    <li><span style="color:green;">Roots 4 month.</span></li>
+    <li>Stem.</li>
+    <li>Meristem.</li>
+    <li>Leaves.</li>
+    <li><span style="color:green;">Leaves 4 month.</span></li>
+    <li>Flowers.</li>
+    <li><span style="color:orange;">Pollen.</span> 3 replicates of mature pollen.</li>
+    <li>Fruits.</li>
+    <li><span style="color:blue;">Seed.</span> Only 1 replicate.</li>
+    <li><span style="color:blue;">Testa+endosperm.</span> 3 replicates.</li>
+    <li><span style="color:blue;">Embryo.</span> 3 replicates.</li>
+  </ul>
+<p>
+  <span style="color:green;">Leaves and Roots</span> (in green) are control samples from 4 month old potted plants in conditions as published by <a href="https://doi.org/10.1093/dnares/dsu033" target="_blank">Leyva-Pérez et al., 2015</a>.
+  <span style="color:blue;">Seed, Embryo and Testa+endosperm</span> (in blue) are control samples in conditions as published by <a href="#" target="_blank">Bullones et al., 2022. In preparation</a>.
+  <span style="color:orange;">Pollen</span> samples correspond to mature pollen in conditions as published by <a href="#" target="_blank">Bullones et al., 2022. In preparation</a>.
+  The rest of the tissues (in black) come from three healthy 10-year-old olive trees under field conditions. They were published by <a href="https://doi.org/10.3390/genes11050544" target="_blank">Ramírez-Tejero et al., 2020</a>.
+</p>
+<p>
+  All samples were normalized to TPM.
+</p>
+
+
---
+++ egdb_custom_text/expr_datasets/02_stresses.php
@@ -0,0 +1,37 @@
+<p>
+  RNA-seq of leaves and roots from 4 month old potted olive plants in control and stess conditions.
+  Abiotic stresses consisted of wounded roots and low temperatures, and biotic estress of infection with the fungal plant pathogen <i>Verticillium dahliae</i>.
+</p>
+<p>
+  Verticillium infection was performed by root-dip inoculation in a conidia suspension of a <i>Verticillium dahliae</i> isolate representative of the so-called defoliating pathotype.
+  Cold stress conditions were 10°C durind the day and 4°C in the night, with 14 hours photoperiod.
+</p>
+
+<h4>Experimental Conditions:</h4>
+<ul>
+  <li>Leaves control.</li>
+  <li>Leaves Verticillium 15d. Leaves infected with Verticillium for 15 days.</li>
+  <li>Leaves root wound 15d. Leaves of plants with wounds in roots, after 15 days.</li>
+  <li>Leaves cold 24h. Leaves after 24 hours in cold conditions.</li>
+  <li>Leaves cold 10d. Leaves after 10 days in cold conditions.</li>
+  <li>Roots control.</li>
+  <li>Roots Verticillium 48h. Roots infected with Verticillium for 48 hours.</li>
+  <li>Roots Verticillium 7d. Roots infected with Verticillium for 7 days.</li>
+  <li>Roots Verticillium 15d. Roots infected with Verticillium for 15 days.</li>
+  <li>Roots wound 8h. Roots wounded, after 8 hours.</li>
+  <li>Roots wound 24h. Roots wounded, after 24 hours.</li>
+  <li>Roots wound 7d. Roots wounded, after 7 days.</li>
+</ul>
+
+
+<p>
+  The plant samples employed for this study were obtained from The plants were obtained from a commercial nursery located in Córdoba province, southern Spain. 
+  Two biological replicates per sample were sequenced.
+  cDNA libraries were sequenced by PE sequencing (100 x 2) with an Illumina HiSeq 1000 sequencer and normalized to TPM.
+</p>
+<p>
+  This dataset was published by <a href="https://doi.org/10.1093/dnares/dsu033" target="_blank">Leyva-Pérez et al. 2014</a> 
+  and raw data can be found in the <a href="https://www.ncbi.nlm.nih.gov/bioproject/PRJNA256033" target="_blank">BioProject PRJNA256033</a>.
+</p>
+
+
