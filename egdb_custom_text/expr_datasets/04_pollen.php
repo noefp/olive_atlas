075bd96f405524a888d190800875cb7074830677
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2022-08-12 16:17:38 +0200

    dataset description

---
+++ egdb_custom_text/expr_datasets/04_pollen.php
@@ -0,0 +1,21 @@
+<p>
+  RNA-seq of pollen tube growth at four times of development. 
+  Pollen grains were pre-hydratated by incubation in a humid chamber at 
+  room temperature for 1 hour and then transferred to Petri dishes containing germination medium. 
+  Petri dishes were maintained at room temperature in the dark and under gentle agitation. 
+  Pollen grains were sampled at 1, 3 and 6 hours after transfer to the in vitro germination medium.
+</p>
+<h4>Experimental Conditions:</h4>
+  <ul>
+    <li>Pollen. Dehydrated mature pollen.</li>
+    <li>Pollen germ 1h. Pollen after 1 hour of germination.</li>
+    <li>Pollen germ 3h. Pollen after 3 hours of germination.</li>
+    <li>Pollen germ 6h. Pollen after 6 hours of germination.</li>
+  </ul>
+<p>
+  Mature pollen grains were collected during the anthesis of adult olive trees growing at 
+  Estación Experimental del Zaidín in Granada, Spain. Three biological replicates per sample were sequenced and normalized to TPM.
+</p>
+<p>
+  This dataset will be published by <a href="#" target="_blank">Bullones et al., 2022 (In preparation)</a>.
+</p>
---
+++ egdb_custom_text/expr_datasets/05_seeds.php
@@ -0,0 +1,20 @@
+<p>
+  RNA-seq of mature seeds sampled from ripe olives.
+  Fruits were manually destoned and seeds were extracted by mechanical breakage of the woody endocarp. 
+  The seed tissues (testa + endosperm and embryo) were dissected with a scalpel.
+</p>
+<h4>Experimental Conditions:</h4>
+  <ul>
+    <li>Seeds. The complete seed.</li>
+    <li>Testa+Endosperm.</li>
+    <li>Embryo.</li>
+  </ul>
+<p>
+  Olive fruits were collected 210 days after anthesis from adult olive trees growing at 
+  the Estación Experimental del Zaidín in Granada, Spain. 
+  One biological replicate of Seed and three biological replicates of Testa+Endosperm and Embryo were sequenced.
+  Data shown were normalized to TPM.
+</p>
+<p>
+  This dataset will be published by <a href="#" target="_blank">??? et al., 2022 (In preparation)</a>.
+</p>
