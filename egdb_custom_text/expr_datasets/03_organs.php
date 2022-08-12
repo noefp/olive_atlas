2192ec1d030e84e4838c3ef7e3384279180167ec
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2022-08-12 12:01:02 +0200

    improved description

--- egdb_custom_text/expr_datasets/03_organs.php
+++ egdb_custom_text/expr_datasets/03_organs.php
@@ -2,16 +2,26 @@
   RNA-seq of roots, stems, meristems, leaves, flowers, and fruits (epicarp and mesocarp)
   of three healthy 10-year-old olive trees under field conditions. All the samples were collected at the same time,
   except for fruits, which had to be collected later upon reaching the turning ripening state. 
-  <br>
-  <br>
+</p>
+<h4>Experimental Conditions (all in control conditions):</h4>
+  <ul>
+    <li>Ripe fruit</li>
+    <li>Flower</li>
+    <li>Leaves</li>
+    <li>Meristem</li>
+    <li>Roots</li>
+    <li>Stem</li>
+  </ul>
+<p>
+
   The plant samples employed for this study were obtained from the World Olive Germplasm Collection (WOGC) 
-  of the Andalusian Institute of Agricultural and Fisheries Research and Training (IFAPA).
+  of the Andalusian Institute of Agricultural and Fisheries Research and Training (IFAPA). 
+  Two biological replicates per sample were sequenced, and each biological replicate consisted in an equilibrated pool of three plant RNAs.
   cDNA libraries were sequenced by PE sequencing (100 x 2) with an Illumina HiSeq 2000 sequencer and normalized to TPM.
-  <br>
-  <br>
-  These dataset was published in <a href="https://doi.org/10.3390/genes11050544" target="_blank">Ramírez-Tejero et al., 2020</a> 
-  and <a href="https://www.ncbi.nlm.nih.gov/bioproject/PRJNA590386/" target="_blank">BioProject PRJNA590386</a>
+</p>
+<p>
+  This dataset was published by <a href="https://doi.org/10.3390/genes11050544" target="_blank">Ramírez-Tejero et al., 2020</a> 
+  and raw data can be found in the <a href="https://www.ncbi.nlm.nih.gov/bioproject/PRJNA590386/" target="_blank">BioProject PRJNA590386</a>.
 </p>
 
-<br>
 
