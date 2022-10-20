34feb5d9776a04aee47cdcb09dd6a38a782b132d
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2022-10-20 09:56:45 +0200

    new dataset description

---
+++ egdb_custom_text/expr_datasets/09_varieties_verticilium.php
@@ -0,0 +1,26 @@
+<p>
+  RNA-seq of roots from 14 to 20 years old olive plants corresponding to 36 olive cultivar
+  in good phytosanitary conditions without any disease symptoms.
+</p>
+
+<h4>Cultivars grouped by their resistance or susceptibility to the fungal plant pathogen <i>Verticillium dahliae</i>:</h4>
+<ul>
+  <li>Highly Resistant: Frantoio, Manzanillera de Huercal Overa.
+  <li>Resistant: Dokkar, Koroneiki, Leccino, Maarri, Maureya, Uslu.
+  <li>Moderately Susceptible: Arbequina, Barnea, Barri, Klon 14, Manzanilla de Sevilla,
+		Morrut, Myrtolia, Picual, Picudo, Piñonera, Verdial de Vélez Málaga.
+  <li>Susceptible: Abbadi Abou Gabra, Abou Salt Mohazam, Chemlal de Kabylie, Llumeta, Menya, Temprano.
+  <li>Extremely Susceptible: Jabali, Mastoidis.
+</ul>
+
+
+<p>
+  The plant samples employed for this study were collected at the World Olive Germplasm Bank at IFAPA Centro 'Alameda del Obispo'
+  located in Córdoba province, Spain.
+  Two biological replicates per sample were sequenced.
+  cDNA libraries were sequenced by PE sequencing (100 x 2) with an Illumina HiSeq 2500 sequencer and normalized to RPKM.
+</p>
+<p>
+  This dataset was published by <a href="https://doi.org/10.1186/s12864-021-07545-x" target="_blank">Ramírez-Tejero et al. 2021</a>
+  and raw data can be found in the <a href="https://www.ncbi.nlm.nih.gov/bioproject/PRJNA638671" target="_blank">BioProject PRJNA638671</a>.
+</p>
