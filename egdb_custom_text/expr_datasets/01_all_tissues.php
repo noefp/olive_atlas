2b20af6bd546aaab7000668e024e255330d33c4b
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2022-10-20 12:17:23 +0200

    Fixed caption in dataset descriptions

--- egdb_custom_text/expr_datasets/01_all_tissues.php
+++ egdb_custom_text/expr_datasets/01_all_tissues.php
@@ -3,32 +3,35 @@
   combined from several experiments.
 </p>
 <h4>Experimental Conditions (all in control conditions with 2 replicates unless otherwise noted):</h4>
-  <ul>
-    <li>Roots.</li>
-    <li><span style="color:green;">Roots 4 month.</span></li>
-    <li>Stem.</li>
-    <li>Meristem.</li>
-    <li>Leaves.</li>
-    <li><span style="color:green;">Leaves 4 month.</span></li>
-    <li>Flowers.</li>
-    <li><span style="color:orange;">Pollen.</span> 3 replicates of mature pollen.</li>
-    <li>Fruits.</li>
-    <li><span style="color:blue;">Seed.</span> Only 1 replicate.</li>
-    <li><span style="color:blue;">Testa+endosperm.</span> 3 replicates.</li>
-    <li><span style="color:blue;">Embryo.</span> 3 replicates.</li>
-  </ul>
+
+<ul>
+  <li>Roots.</li>
+  <li><span style="color:green;">Roots 4 month.</span></li>
+  <li>Stem.</li>
+  <li>Meristem.</li>
+  <li>Leaves.</li>
+  <li><span style="color:green;">Leaves 4 month.</span></li>
+  <li>Flowers.</li>
+  <li><span style="color:orange;">Pollen.</span> 3 replicates of mature pollen.</li>
+  <li>Fruits.</li>
+  <li><span style="color:blue;">Seed.</span> Only 1 replicate.</li>
+  <li><span style="color:blue;">Testa+endosperm.</span> 3 replicates.</li>
+  <li><span style="color:blue;">Embryo.</span> 3 replicates.</li>
+</ul>
+
+  
+<img class='rounded' src='<?php echo "$images_path/expr/roots_4_months.jpeg";?>' width="200px" alt='roots 4 months'>
+<img class='rounded' src='<?php echo "$images_path/expr/picual_stem.jpeg";?>' width="200px" alt='stem'>
+<img class='rounded' src='<?php echo "$images_path/expr/picual_meristem.jpeg";?>' width="200px" alt='meristem'>
+<img class='rounded' src='<?php echo "$images_path/expr/picual_leaves.jpeg";?>' width="200px" alt='leaves'>
+<img class='rounded' src='<?php echo "$images_path/expr/leaves_control.jpeg";?>' width="200px" alt='leaves 4 month'>
+<img class='rounded' src='<?php echo "$images_path/expr/picual_flowers.jpeg";?>' width="200px" alt='flowers'>
+<img class='rounded' src='<?php echo "$images_path/expr/picual_fruits.jpeg";?>' width="200px" alt='olives'>
+  
 <p>
-  
-  <img class='rounded' src='<?php echo "$images_path/expr/roots_4_months.jpeg";?>' width="200px" alt='roots 4 months'>
-  <img class='rounded' src='<?php echo "$images_path/expr/picual_stem.jpeg";?>' width="200px" alt='stem'>
-  <img class='rounded' src='<?php echo "$images_path/expr/picual_meristem.jpeg";?>' width="200px" alt='meristem'>
-  <img class='rounded' src='<?php echo "$images_path/expr/picual_leaves.jpeg";?>' width="200px" alt='leaves'>
-  <img class='rounded' src='<?php echo "$images_path/expr/leaves_control.jpeg";?>' width="200px" alt='leaves 4 month'>
-  <img class='rounded' src='<?php echo "$images_path/expr/picual_flowers.jpeg";?>' width="200px" alt='flowers'>
-  <img class='rounded' src='<?php echo "$images_path/expr/picual_fruits.jpeg";?>' width="200px" alt='olives'>
-  
   From left to right are shown figures of Picual 4 month old roots, stem, meristem, leaves, 4 month old leaves, flowers, and fruits.
-  
+</p>
+<p>
   <span style="color:green;">Leaves and Roots</span> (in green) are control samples from 4 month old potted plants in conditions as published by <a href="https://doi.org/10.1093/dnares/dsu033" target="_blank">Leyva-Pérez et al., 2015</a>.
   <span style="color:blue;">Seed, Embryo and Testa+endosperm</span> (in blue) are control samples in conditions as published by <a href="#" target="_blank">??? et al., 2022. In preparation</a>.
   <span style="color:orange;">Pollen</span> samples correspond to mature pollen in conditions as published by <a href="#" target="_blank">Bullones et al., 2022. In preparation</a>.
