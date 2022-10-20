d01ceab048f81203c4c10c503a967ab8dee9e9a5
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2022-10-20 09:05:33 +0200

    modified oliveAtlas citation

--- egdb_custom_text/db_citation.php
+++ egdb_custom_text/db_citation.php
@@ -4,9 +4,9 @@
     <h4>Please, when using this web site or its data, cite us using the reference:</h4>
 
     <div class="card bg-light" style="height:100px">
-      <div class= "card-body">    
-        Bullones A, Guedes A, Alché JD, Luque F, Claros MG, Fernandez-Pozo N. (2022).
-        OliveAtlas: a gene expression atlas for Olea europaea.<br>
+      <div class= "card-body">
+        Amanda Bullones, Antonio Jesús Castro, Elena Lima-Cabello, Juan de Dios Alché, Francisco Luque, M. Gonzalo Claros, Noe Fernandez-Pozo (2022).
+        OliveAtlas: a gene expression atlas for <i>Olea europaea</i>.<br>
         <a href="paper_link" target="blank">Journal, 1(1), 111</a><br>
       </div>
     </div>
