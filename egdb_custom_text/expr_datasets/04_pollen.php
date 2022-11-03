a6a2b6212936d80027d3ec33d64ea1dd1cac40e5
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2022-11-03 13:13:34 +0100

    photos added to description

--- egdb_custom_text/expr_datasets/04_pollen.php
+++ egdb_custom_text/expr_datasets/04_pollen.php
@@ -6,12 +6,23 @@
   Pollen grains were sampled at 1, 3 and 6 hours after transfer to the in vitro germination medium.
 </p>
 <h4>Experimental Conditions:</h4>
-  <ul>
-    <li>Pollen. Dehydrated mature pollen.</li>
-    <li>Pollen germ 1h. Pollen after 1 hour of germination.</li>
-    <li>Pollen germ 3h. Pollen after 3 hours of germination.</li>
-    <li>Pollen germ 6h. Pollen after 6 hours of germination.</li>
-  </ul>
+
+<ul>
+  <li>Pollen. Rehydrated mature pollen.</li>
+  <li>Pollen germ 1h. Pollen after 1 hour of germination.</li>
+  <li>Pollen germ 3h. Pollen after 3 hours of germination.</li>
+  <li>Pollen germ 6h. Pollen after 6 hours of germination.</li>
+</ul>
+  
+<img class='rounded' src='<?php echo "$images_path/expr/picual_pollen_0.jpeg";?>' width="350px" alt='pollen'>
+<img class='rounded' src='<?php echo "$images_path/expr/picual_pollen_1.jpeg";?>' width="350px" alt='pollen'>
+<img class='rounded' src='<?php echo "$images_path/expr/picual_pollen_2.jpeg";?>' width="350px" alt='pollen'>
+<img class='rounded' src='<?php echo "$images_path/expr/picual_pollen_3.jpeg";?>' width="350px" alt='pollen'>
+
+<p>
+  Images of pollen tube growth. From left to right, rehydrated mature pollen, and pollen tube growth at 1, 3 and 6 hours after of germination
+</p>
+
 <p>
   Mature pollen grains were collected during the anthesis of adult olive trees growing at 
   Estación Experimental del Zaidín in Granada, Spain. Three biological replicates per sample were sequenced and normalized to TPM.
