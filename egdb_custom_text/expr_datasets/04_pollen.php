c6d160d3c7036459dbb5544e2f2a2ffaeeea95ad
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2022-12-23 08:53:50 +0100

    mature pollen

--- egdb_custom_text/expr_datasets/04_pollen.php
+++ egdb_custom_text/expr_datasets/04_pollen.php
@@ -8,7 +8,7 @@
 <h4>Experimental Conditions:</h4>
 
 <ul>
-  <li>Pollen. Rehydrated mature pollen.</li>
+  <li>Pollen. Mature pollen.</li>
   <li>Pollen germ 1h. Pollen after 1 hour of germination.</li>
   <li>Pollen germ 3h. Pollen after 3 hours of germination.</li>
   <li>Pollen germ 6h. Pollen after 6 hours of germination.</li>
@@ -20,7 +20,7 @@
 <img class='rounded' src='<?php echo "$images_path/expr/picual_pollen_3.jpeg";?>' width="200px" alt='pollen'>
 
 <p>
-  Images of pollen tube growth. From left to right, rehydrated mature pollen, and pollen tube growth at 1, 3 and 6 hours after of germination.
+  Images of pollen tube growth. From left to right, mature pollen, and pollen tube growth at 1, 3 and 6 hours after of germination.
 </p>
 
 <p>
