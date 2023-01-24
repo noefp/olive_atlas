741c530f8f9526ae989e7797e47f9f9c023b917e
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2023-01-24 10:27:06 +0100

    added institution images to about

--- egdb_custom_text/about.php
+++ egdb_custom_text/about.php
@@ -1,7 +1,7 @@
 <p class="p_font18">
   The <b>OliveAtlas</b> was developed by researchers from three Spanish research centers: the <a href="https://www.ihsm.uma-csic.es/" target="_blank">Institute for Mediterranean and Subtropical Horticulture "La Mayora" (IHSM-UMA-CSIC)</a>, 
-  the <a href="https://www.ujaen.es/en" target="_blank">Center for Advanced Studies in Olive Grove and Olive Oils, Universidad de Jaén</a> 
-  and the <a href="https://www.eez.csic.es/" target="_blank">Estación Experimental del Zaidín (EEZ-CSIC)</a>.
+  the <a href="https://www.eez.csic.es/" target="_blank">Estación Experimental del Zaidín (EEZ-CSIC)</a> and 
+  the <a href="https://inuouja.com/en/home-2/" target="_blank">Center for Advanced Studies in Olive Grove and Olive Oils, Universidad de Jaén (INUOUJA)</a>.
 </p>
 <br>
 <p>
@@ -13,13 +13,31 @@
 
 
 <center>
-  <img class='rounded' src='<?php echo "$images_path/header_img.jpg";?>' width="80%" alt='img example'>
-  <p>
-    OliveAtlas was developed at the The Institute for Mediterranean and Subtropical Horticulture "La Mayora" (IHSM-UMA-CSIC).
-  </p>
+  <div class="row">
+    <div class="col">
+      <a href="https://www.ihsm.uma-csic.es/" target="_blank"><img class='rounded' src='<?php echo "$images_path/IHSM.jpg";?>' width="100%" alt='IHSM-UMA-CSIC'></a>
+    </div>
+    <div class="col">
+      <a href="https://www.eez.csic.es/" target="_blank"><img class='rounded' src='<?php echo "$images_path/EEZ.jpeg";?>' width="100%" alt='EEZ-CSIC'></a>
+    </div>
+    <div class="col">
+      <a href="https://inuouja.com/en/home-2/" target="_blank"><img class='rounded' src='<?php echo "$images_path/UJA.jpg";?>' width="100%" alt='INUOUJA'></a>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col">
+      IHSM-UMA-CSIC
+    </div>
+    <div class="col">
+      EEZ-CSIC
+    </div>
+    <div class="col">
+      INUOUJA
+    </div>
+  </div>
 </center>
-<br>
-<br>
+
+<hr>
 <h2>Funding</h2>
 <p>
   This project was possible thanks to the next funding agencies and projects: 
@@ -32,8 +50,7 @@
   <img class='rounded funding_logo' src='<?php echo "$images_path/logos/RyC_logo.png";?>' height="90px" alt='Logo financiacion RyC'>
   <img class='rounded funding_logo' src='<?php echo "$images_path/logos/csic_logo.png";?>' height="70px" alt='Logo CSIC'>
 </div>
-<br>
-<br>
+<hr>
 <h2>How to cite OliveAtlas</h2>
 
 
