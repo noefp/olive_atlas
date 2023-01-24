cd880d313a6e7332739cb53119c2ebced5ddffb7
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2023-01-24 10:31:14 +0100

    justified paragraphs

--- egdb_custom_text/welcome_text.php
+++ egdb_custom_text/welcome_text.php
@@ -3,13 +3,13 @@
         The gene expression atlas for Olive tree (<i>Olea europaea L.</i>)
       </h4>
       <br>
-      <p>
+      <p style="text-align:justify">
         OliveAtlas aims to provide interactive bioinformatics tools to explore gene expression data of olive tree.
         The current version of OliveAtlas includes data for most of the main organs of the olive tree variety Picual
         (leaves, flower, pollen, fruits, seeds, roots, stem, and meristem),
         and multiple responses to biotic and abiotic stresses such as cold, wound and infection of <i>Verticillium dahliaea</i>.
       </p>
-      <p>
+      <p style="text-align:justify">
         All the Picual expression data were analyzed based on the olive tree genome sequence published by <a href="https://doi.org/10.1002/tpg2.20010" target="_blank">Jiménez-Ruiz et al., 2020</a> 
         and all data are linked to sequences and annotations available at the <a href="https://genomaolivar.dipujaen.es/" target="_blank">OliveTreeDB</a>.
       </p>
