dc504659087c2ab7aabe07d3421696b976e7c4d3
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2023-01-24 10:30:03 +0100

    reordered menu

--- egdb_custom_text/custom_toolbar.php
+++ egdb_custom_text/custom_toolbar.php
@@ -1,6 +1,6 @@
 <li class="nav-item"><a class="nav-link" href="/easy_gdb/tools/expression/expression_input.php"><span style="color:#5d5">Expression Viewer</span></a></li>
-<li class="nav-item"><a class="nav-link" href="/easy_gdb/tools/gene_lookup.php">Gene Lookup</a></li>
 <li class="nav-item"><a class="nav-link" href="/easy_gdb/tools/expression/expression_menu.php">Datasets</a></li>
+<li class="nav-item"><a class="nav-link" href="/easy_gdb/tools/gene_lookup.php">Gene Lookup</a></li>
 <li class="nav-item dropdown">
   <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">OliveTreeDB</a>
   <div class="dropdown-menu">
