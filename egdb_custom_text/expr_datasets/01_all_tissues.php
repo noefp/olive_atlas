bd44cc40f4935d4f90169ce2cbd125926cbd9e4e
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2023-01-25 13:49:12 +0100

    removed unpublished samples

--- egdb_custom_text/expr_datasets/01_all_tissues.php
+++ egdb_custom_text/expr_datasets/01_all_tissues.php
@@ -15,8 +15,8 @@
   <li><span style="color:orange;">Pollen.</span> 3 replicates of mature pollen.</li>
   <li>Fruits: Reaching the turning ripening state, veraison.</li>
   <li><span style="color:blue;">Seed.</span> Mature seed (whole), 1 replicate.</li>
-  <li><span style="color:blue;">Testa+endosperm.</span> Endosperm and seed coat (testa), 3 replicates.</li>
-  <li><span style="color:blue;">Embryo.</span> Mature embryo, 3 replicates.</li>
+  <!-- <li><span style="color:blue;">Testa+endosperm.</span> Endosperm and seed coat (testa), 3 replicates.</li>
+  <li><span style="color:blue;">Embryo.</span> Mature embryo, 3 replicates.</li> -->
 </ul>
 
   
@@ -29,16 +29,17 @@
 <img class='rounded' src='<?php echo "$images_path/expr/picual_pollen_0.jpeg";?>' width="200px" alt='pollen'>
 <img class='rounded' src='<?php echo "$images_path/expr/picual_fruits.jpeg";?>' width="200px" alt='olives'>
 <img class='rounded' src='<?php echo "$images_path/expr/picual_seeds.jpeg";?>' width="200px" alt='olive seeds'>
-<img class='rounded' src='<?php echo "$images_path/expr/picual_testa.jpeg";?>' width="200px" alt='olive testa'>
-<img class='rounded' src='<?php echo "$images_path/expr/picual_embryos.jpeg";?>' width="200px" alt='olive embryos'>
+<!-- <img class='rounded' src='<?php //echo "$images_path/expr/picual_testa.jpeg";?>' width="200px" alt='olive testa'>
+<img class='rounded' src='<?php //echo "$images_path/expr/picual_embryos.jpeg";?>' width="200px" alt='olive embryos'> -->
   
 <p>
   From left to right are shown figures of Picual 4 month old roots, stem, meristem, leaves, 4 month old leaves, flowers, rehydrated mature pollen, fruits, seeds, seed testa + endosperm, and embryos.
 </p>
 <p>
   <span style="color:green;">Leaves and Roots</span> (in green) are control samples from 4 month old potted plants in conditions as published by <a href="https://doi.org/10.1093/dnares/dsu033" target="_blank">Leyva-Pérez et al., 2015</a>.
-  <span style="color:blue;">Seed, Embryo and Testa+endosperm</span> (in blue) are control samples in conditions as published by <a href="#" target="_blank">??? et al., 2022. In preparation</a>.
-  <span style="color:orange;">Pollen</span> samples correspond to mature pollen in conditions as published by <a href="#" target="_blank">Bullones et al., 2022. In preparation</a>.
+  <span style="color:blue;">Seed</span> (in blue) is a control sample</a>.
+  <!-- <span style="color:blue;">Seed, Embryo and Testa+endosperm</span> (in blue) are control samples in conditions are in preparation</a>. -->
+  <span style="color:orange;">Pollen</span> samples correspond to mature rehydrated pollen.
   The rest of the tissues (in black) come from three healthy 10-year-old olive trees under field conditions. They were published by <a href="https://doi.org/10.3390/genes11050544" target="_blank">Ramírez-Tejero et al., 2020</a>.
 </p>
 <p>
