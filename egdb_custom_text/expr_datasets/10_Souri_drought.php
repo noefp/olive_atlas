ecd0cea7f6c768d520fabbdfd5de9ef41d639ef1
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2023-02-22 13:25:44 +0100

    new dataset description

---
+++ egdb_custom_text/expr_datasets/10_Souri_drought.php
@@ -0,0 +1,29 @@
+<p>
+	RNA-seq of wood of twelve 3-year-old olive plants corresponding to Souri cultivar in pots under 90 days of seasonal spring to summer warming.
+	Six trees out of twelve were exposed to limited water conditions for 28 days. The six watered trees were irrigated continuosly.
+	Samples were collected at four time-points: day -3 (baseline), day 13 (heat wave and early drought), day 27 (late drought) and day 80 (post-drought).
+</p>
+
+
+<h4>Experimental Conditions (3 biological replicates):</h4>
+<ul>
+  <li>Watered trees -3d.
+	<li>Watered trees 13d.
+	<li>Watered trees 27d.
+	<li>Watered trees 80d.
+	<li>Dried trees -3d.
+	<li>Dried trees 13d.
+	<li>Dried trees 27d.
+	<li>Dried trees 80d.
+</ul>
+
+
+<p>
+	The plant samples employed in this study were collected at the Weizmann Institute of Science located in Rehovot, Israel.
+	Three biological replicates per sample were sequenced.
+	cDNA libraries were sequenced by PE sequencing (100 x 2) with an Illumina HiSeq 2500 sequencer and normalized to RPKM.
+</p>
+<p>
+  This dataset was published by <a href="https://doi.org/10.1111/nph.16907" target="_blank">Tsamir-Rimon et al. 2020</a>
+  and raw data can be found in the <a href="https://www.ncbi.nlm.nih.gov/bioproject/PRJNA606032" target="_blank">BioProject PRJNA606032</a>.
+</p>
