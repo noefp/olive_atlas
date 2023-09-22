15aac954373946599890978b9f182dae038d94ae
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2023-09-22 13:29:04 +0200

    new dataset descriptions

--- egdb_custom_text/expr_datasets/01_all_tissues.php
+++ egdb_custom_text/expr_datasets/01_all_tissues.php
@@ -6,20 +6,21 @@
 
 <ul>
   <li>Roots.</li>
-  <li><span style="color:green;">Roots 4 month.</span></li>
+  <li><span style="color:green;">Roots 4 months</span>.</li>
   <li>Stem.</li>
   <li>Meristem.</li>
   <li>Leaves.</li>
-  <li><span style="color:green;">Leaves 4 month.</span></li>
+  <li><span style="color:green;">Leaves 4 months</span>.</li>
   <li>Flowers: Recently bloomed whole flowers (at anthesis).</li>
-  <li><span style="color:orange;">Pollen.</span> 3 replicates of mature pollen.</li>
+  <li><span style="color:orange;">Pollen mature</span>: 3 replicates of mature pollen.</li>
+	<li><span style="color:orange;">Pollen germ 6h</span>: 3 replicates of pollen after 6 hours of germination.</li>
   <li>Fruits: Reaching the turning ripening state, veraison.</li>
-  <li><span style="color:blue;">Seed.</span> Mature seed (whole), 1 replicate.</li>
+  <li><span style="color:blue;">Seed</span>: Mature seed (whole), 1 replicate.</li>
   <!-- <li><span style="color:blue;">Testa+endosperm.</span> Endosperm and seed coat (testa), 3 replicates.</li>
   <li><span style="color:blue;">Embryo.</span> Mature embryo, 3 replicates.</li> -->
 </ul>
 
-  
+
 <img class='rounded' src='<?php echo "$images_path/expr/roots_4_months.jpeg";?>' width="200px" alt='roots 4 months'>
 <img class='rounded' src='<?php echo "$images_path/expr/picual_stem.jpeg";?>' width="200px" alt='stem'>
 <img class='rounded' src='<?php echo "$images_path/expr/picual_meristem.jpeg";?>' width="200px" alt='meristem'>
@@ -31,7 +32,7 @@
 <img class='rounded' src='<?php echo "$images_path/expr/picual_seeds.jpeg";?>' width="200px" alt='olive seeds'>
 <!-- <img class='rounded' src='<?php //echo "$images_path/expr/picual_testa.jpeg";?>' width="200px" alt='olive testa'>
 <img class='rounded' src='<?php //echo "$images_path/expr/picual_embryos.jpeg";?>' width="200px" alt='olive embryos'> -->
-  
+
 <p>
   From left to right are shown figures of Picual 4 month old roots, stem, meristem, leaves, 4 month old leaves, flowers, rehydrated mature pollen, fruits, seeds, seed testa + endosperm, and embryos.
 </p>
@@ -39,11 +40,9 @@
   <span style="color:green;">Leaves and Roots</span> (in green) are control samples from 4 month old potted plants in conditions as published by <a href="https://doi.org/10.1093/dnares/dsu033" target="_blank">Leyva-Pérez et al., 2015</a>.
   <span style="color:blue;">Seed</span> (in blue) is a control sample</a>.
   <!-- <span style="color:blue;">Seed, Embryo and Testa+endosperm</span> (in blue) are control samples in conditions are in preparation</a>. -->
-  <span style="color:orange;">Pollen</span> samples correspond to mature rehydrated pollen.
+  <span style="color:orange;">Pollen</span> samples correspond to mature rehydrated pollen, and <span style="color:orange;">Pollen germ 6h</span> correspond to pollen after 6 hours of germination as published by <a href="https://doi.org/10.3390/plants12162894" target="_blank">Bullones et al., 2023</a>.
   The rest of the tissues (in black) come from three healthy 10-year-old olive trees under field conditions. They were published by <a href="https://doi.org/10.3390/genes11050544" target="_blank">Ramírez-Tejero et al., 2020</a>.
 </p>
 <p>
   All samples were normalized to TPM.
 </p>
-
-
--- egdb_custom_text/expr_datasets/03_pollen.php
+++ egdb_custom_text/expr_datasets/03_pollen.php
@@ -1,32 +1,32 @@
 <p>
-  RNA-seq of pollen tube growth at four times of development. 
-  Pollen grains were pre-hydratated by incubation in a humid chamber at 
-  room temperature for 1 hour and then transferred to Petri dishes containing germination medium. 
-  Petri dishes were maintained at room temperature in the dark and under gentle agitation. 
+  RNA-seq of pollen tube growth at four times of development.
+  Pollen grains were pre-hydratated by incubation in a humid chamber at
+  room temperature for 1 hour and then transferred to Petri dishes containing germination medium.
+  Petri dishes were maintained at room temperature in the dark and under gentle agitation.
   Pollen grains were sampled at 1, 3 and 6 hours after transfer to the in vitro germination medium.
 </p>
 <h4>Experimental Conditions (3 biological replicates):</h4>
 
 <ul>
-  <li>Pollen. Mature pollen.</li>
+  <li>Pollen mature. Mature rehydrated pollen.</li>
   <li>Pollen germ 1h. Pollen after 1 hour of germination.</li>
   <li>Pollen germ 3h. Pollen after 3 hours of germination.</li>
   <li>Pollen germ 6h. Pollen after 6 hours of germination.</li>
 </ul>
-  
+
 <img class='rounded' src='<?php echo "$images_path/expr/picual_pollen_0.jpeg";?>' width="200px" alt='pollen'>
 <img class='rounded' src='<?php echo "$images_path/expr/picual_pollen_1.jpeg";?>' width="200px" alt='pollen'>
 <img class='rounded' src='<?php echo "$images_path/expr/picual_pollen_2.jpeg";?>' width="200px" alt='pollen'>
 <img class='rounded' src='<?php echo "$images_path/expr/picual_pollen_3.jpeg";?>' width="200px" alt='pollen'>
 
 <p>
-  Images of Picual pollen grains at different stages of in vitro germination. From left to right, mature rehydrated pollen, and germinated pollen tat 1, 3 and 6 hours of culture.
+  Images of Picual pollen grains at different stages of in vitro germination. From left to right, mature rehydrated pollen, and germinated pollen at 1, 3 and 6 hours of culture.
 </p>
 
 <p>
-  Mature pollen grains were collected during the anthesis of adult olive trees growing at 
+  Mature pollen grains were collected during the anthesis of adult olive trees growing at
   Estación Experimental del Zaidín in Granada, Spain. Three biological replicates per sample were sequenced and normalized to TPM.
 </p>
 <p>
-  This dataset will be published by <a href="#" target="_blank">Bullones et al., 2022 (In preparation)</a>.
+  This dataset was published by <a href="https://doi.org/10.3390/plants12162894" target="_blank">Bullones et al., 2023</a>.
 </p>
