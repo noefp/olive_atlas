1bb5545078b4b9d8b507fecb8f0eed68c834ebdc
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2025-02-24 17:46:08 +0100

    margin and cite fixed for new index

--- egdb_custom_text/welcome_text.php
+++ egdb_custom_text/welcome_text.php
@@ -1,4 +1,5 @@
-      <h1>Welcome to <?php echo "$dbTitle";?> </h1>
+	<div class="width900" style="margin-top:20px">
+			<h1>Welcome to <?php echo "$dbTitle";?> </h1>
       <h4 class="p_font18">
         The gene expression atlas for Olive tree (<i>Olea europaea L.</i>)
       </h4>
@@ -10,7 +11,11 @@
         and multiple responses to biotic and abiotic stresses such as cold, wound and infection of <i>Verticillium dahliaea</i>.
       </p>
       <p style="text-align:justify">
-        All the Picual expression data were analyzed based on the olive tree genome sequence published by <a href="https://doi.org/10.1002/tpg2.20010" target="_blank">Jiménez-Ruiz et al., 2020</a> 
+        All the Picual expression data were analyzed based on the olive tree genome sequence published by <a href="https://doi.org/10.1002/tpg2.20010" target="_blank">Jiménez-Ruiz et al., 2020</a>
         and all data are linked to sequences and annotations available at the <a href="https://genomaolivar.dipujaen.es/" target="_blank">OliveTreeDB</a>.
       </p>
       <img class='rounded' src='<?php echo "$images_path/header_img7.jpg";?>' style="width: 100%; max-width: 900px;" alt='img example'>
+			<br>
+			<br>
+			<?php include_once realpath("$custom_text_path/db_citation.php");?>
+	</div>
